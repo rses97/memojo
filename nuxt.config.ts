397f69b3d0a3f42114de871dae8873c5ff151ba6
Rostyslav Ses <rostislavses97@gmail.com>
2026-03-27 10:52:00 +0200

    chore: scaffold Nuxt 4 project with Tailwind v4, Pinia, Vitest

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/test-utils/module',
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
